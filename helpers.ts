48b02e3d22852318b980fac28dd05ac254bd4c2e
legeend <136790200+conrro@users.noreply.github.com>
2025-11-04 00:40:42 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T21:40:24.505Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-03T21:40:40.739Z
+Commit: Patch security vulnerability in authentication
