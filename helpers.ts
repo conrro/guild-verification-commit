f34ef3955457a77538645daf59e0562b9f2e9ecc
legeend <136790200+conrro@users.noreply.github.com>
2025-11-04 00:40:25 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T20:26:47.860Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-03T21:40:24.505Z
+Commit: Fix CSS styling bug on mobile devices
