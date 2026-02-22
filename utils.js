26de35d3afa1d565cd4342275ec6b7b32fcf236b
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:06:41 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:06:38.536Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-22T01:06:40.808Z
+Commit: Fix broken links in documentation
