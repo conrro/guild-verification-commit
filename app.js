3d51c31e1f78547cd3e99a897b1677ec2828c3d1
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:05:38 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:05:35.743Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-22T01:05:37.498Z
+Commit: Fix CSS styling bug on mobile devices
