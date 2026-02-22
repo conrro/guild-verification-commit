b497299ce4a2eed46c3c3200caa840a395e181a6
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:04:31 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:04:20.828Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-02-22T01:04:30.776Z
+Commit: Fix broken links in documentation
