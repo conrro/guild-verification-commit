22599a5fefc4e53f963efeaecb7e86fc578e6c70
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:06:33 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:06:26.970Z
-Commit: Implement user profile customization
+Timestamp: 2026-02-22T01:06:32.053Z
+Commit: Fix memory leak in event handlers
