2be3ce6919b203839c8bcca4e195e9d732440178
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:01:50 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:01:41.415Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-22T01:01:49.459Z
+Commit: Introduce real-time notifications
