293cabfedd1223e4fde086b9cd3abd0a8408663e
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:02:31 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:02:24.774Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-22T01:02:30.817Z
+Commit: Resolve timezone issues in date handling
