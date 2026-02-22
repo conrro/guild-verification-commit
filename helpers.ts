376e45f2f481005f1ba04f09a28f489e6ebe138b
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:01:52 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:01:49.459Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-02-22T01:01:51.228Z
+Commit: Fix memory leak in event handlers
