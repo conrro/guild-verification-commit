1983c5708fdb46649a8fca5ea660b7fbe79e2bc7
legeend <136790200+conrro@users.noreply.github.com>
2026-02-22 04:05:43 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T01:05:37.498Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-02-22T01:05:42.614Z
+Commit: Fix memory leak in event handlers
