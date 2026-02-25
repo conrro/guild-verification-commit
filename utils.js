248d9d7bc728aec05378068d5f410ba8c4ec65fc
legeend <136790200+conrro@users.noreply.github.com>
2026-02-25 12:36:50 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-25T09:35:33.057Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-25T09:36:49.598Z
+Commit: Fix race condition in async operations
